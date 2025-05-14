0ed52b20410dbaf97413116ec69b89c877c88a53
Luis R <78448904+Starmise@users.noreply.github.com>
2025-05-14 08:53:00 -0600

    VS Project created

---
+++ SeguridadVJ/SeguridadVJ/include/CaesarEncryption.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Prerequisites.h"
+
+class
+  CaesarEncryption {
+public:
+  CaesarEncryption() = default;
+  ~CaesarEncryption() = default;
+
+  std::string
+  Encryption(const std::string& texto, int desplazamiento) {
+    return "";
+  }
+
+private:
+
+};
