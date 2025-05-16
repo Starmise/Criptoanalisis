2112a25d78b1b66d4046f264404be4234d4cbb9d
Luis R <78448904+Starmise@users.noreply.github.com>
2025-05-16 08:53:21 -0600

    2nd Attempt and Class Answer for Caesar Encryption
    -Encode
    -Decode
    -BruteForce
    -Possible key evaluation

--- SeguridadVJ/SeguridadVJ/include/CaesarEncryption.h
+++ SeguridadVJ/SeguridadVJ/include/CaesarEncryption.h
@@ -2,23 +2,83 @@
 #include "Prerequisites.h"
 
 class
-CaesarEncryption {
+  CaesarEncryption {
 public:
   CaesarEncryption() = default;
   ~CaesarEncryption() = default;
 
   string
   EncryptionCaesar(const string& texto, int desplazamiento) {
-    for (int i = 0; i < texto.size(); i++) {
+    std::string result = "";
+
+    for (char c : texto) {
+      if (c >= 'A' && c <= 'Z') {
+        result += (char)(((c - 'A' + desplazamiento) % 26) + 'A');
+      }
+      else if (c >= 'a' && c <= 'z') {
+        result += (char)(((c - 'a' + desplazamiento) % 26) + 'a');
+      }
+      else if (c >= '0' && c <= '9') {
+        result += (char)(((c - '0' + desplazamiento) % 10) + '0');
+      }
+      else {
+        result += c;
+      }
+    }
+    return result;
+  }
+
+  string
+  DecodeCaesar(const string& texto, int desplazamiento) {
+    return EncryptionCaesar(texto, 26 - (desplazamiento % 26));
+  }
+
+  void
+  bruteForce(const string& texto) {
+    for (int i = 0; i < 26; i++) {
+      string attempt = EncryptionCaesar(texto, 26 - i);
+      cout << "Inteto: " << i << ": " << attempt << endl;
+    }
+  }
+
+
+  int
+  evaluatePossibleKey(const std::string& texto) {
+    int frecuencias[26] = { 0 };
+
+    for (char c : texto) {
+      if (c >= 'A' && c <= 'Z') {
+        frecuencias[c - 'A']++;
+      }
+      else if (c >= 'a' && c <= 'z') {
+      
+      frecuencias[c - 'a']++;
+      }
+    }
+
+    int indexMax = 0;
+    for (int i = 0; i < 26; i++) {
+      if (frecuencias[i] > frecuencias[0]) {
+        indexMax = i;
+      }
+    }
+
+    int possibleKey = (indexMax - ('e' - 'a') + 26) % 26;
+    return possibleKey;
+  }
+
+private:
+
+};
+
+/*
+* ----------2nd Attempt-----------
+* for (int i = 0; i < texto.size(); i++) {
       char posActual = texto[i];
-      char posNueva = (posActual + desplazamiento) % alfabeto.size();
+
+      char posNueva = ((posActual + desplazamiento)) % alfabeto.size() + 'a';
 
       respuesta += posNueva;
     }
     return respuesta;
-  }
-
-private:
-  string alfabeto = "abcdefghijklmnopqrstuvwxyz";
-  string respuesta = "";
-};
+*/
--- SeguridadVJ/SeguridadVJ/src/main.cpp
+++ SeguridadVJ/SeguridadVJ/src/main.cpp
@@ -3,13 +3,20 @@
 int 
 main() {
   string texto;
+  int desplazo;
 
   cout << "Escribe el mensaje a encriptar: " << endl;;
-  cin >> texto;
+  getline(cin, texto);
+
+  cout << "Escribe el valor de desplazamiento: " << endl;;
+  cin >> desplazo;
 
   CaesarEncryption cifrado;
-  string mensjaeCifrado = cifrado.EncryptionCaesar(texto, 4);
+  string mensjaeCifrado = cifrado.EncryptionCaesar(texto, desplazo);
   cout << "El mensaje cifrado es: " << mensjaeCifrado << endl;
+
+  string descifrado = cifrado.DecodeCaesar(mensjaeCifrado, desplazo);
+  cout << "El mensaje descifrado es: " << descifrado << endl;
 
   return 0;
 }
