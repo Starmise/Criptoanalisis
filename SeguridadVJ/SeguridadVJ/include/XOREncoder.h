accec686e151c94436bec7e55949371e09831117
Luis R <78448904+Starmise@users.noreply.github.com>
2025-05-23 08:55:06 -0600

    XOR Implemented

--- SeguridadVJ/SeguridadVJ/include/XOREncoder.h
+++ SeguridadVJ/SeguridadVJ/include/XOREncoder.h
@@ -1,8 +1,8 @@
 #pragma once
 #include "Prerequisites.h"
 
-class XOREncoder
-{
+class 
+XOREncoder {
 public:
   XOREncoder() = default;
   ~XOREncoder() = default;
@@ -21,7 +21,7 @@ public:
   }
 
   // Transform text to decimal(hex) representation
-  vector<unsigned char> 
+  vector<unsigned char>
   HexToBytes(const string& input) {
     vector<unsigned char> bytes;
     istringstream iss(input);
@@ -39,6 +39,81 @@ public:
     }
     return bytes;
   }
+
+  void
+  printHex(const string& input) {
+    for (unsigned char c : input) {
+      cout << hex << setw(2) << setfill('0') << (int)c << " ";
+    }
+  }
+
+  bool 
+  isValidText(const string& data) {
+    return all_of(data.begin(), data.end(), [](unsigned char c) {
+      return isprint(c) || isspace(c) || c == '\n' || c == ' ';
+      });
+  }
+
+  void
+  bruteForce_1Byte(const vector<unsigned char>& cifrado) {
+    for (int clave = 0; clave < 256; clave++) {
+      string result;
+
+      for (unsigned char c : cifrado) {
+        result += c ^ clave;
+      }
+
+      if (isValidText(result)) {
+        cout << "============================\n";
+        cout << "Clave 1 bytes: " << static_cast<char>(clave)
+          << "' (0x" << std::hex << std::setw(2) << std::setfill('0') << clave << ")\n";
+        cout << "Texto posible: " << result << "\n";
+      }
+    }
+  }
+
+  void
+  bruteForce_2Byte(const vector<unsigned char>& cifrado) {
+    for (int b1 = 0; b1 < 256; b1++) {
+      for (int b2 = 0; b2 < 256; b2++) {
+        string result;
+        unsigned char key[2] = { static_cast<unsigned char>(b1), static_cast<unsigned char>(b2) };
+
+        for (int i = 0; i < cifrado.size(); i++) {
+          result += cifrado[i] ^ key[i % 2];
+        }
+        
+        if (isValidText(result)) {
+          cout << "============================\n";
+          cout << "Clave 2 bytes: " << static_cast<char>(b1) << static_cast<char>(b2)
+            << "' (0x" << std::hex << std::setw(2) << std::setfill('0') << b1
+            << "0x " << std::setw(2) << std::setfill('0') << b2 << ")\n";
+          cout << "Texto posible: " << result << "\n";
+        }
+      }
+    }
+  }
+
+  void bruteForceByDictionary(const std::vector<unsigned char>& cifrado) {
+    std::vector<std::string> clavesComunes = {
+      "clave", "admin", "1234", "root", "test", "abc", "hola", "user",
+      "pass", "12345", "0000", "password", "default", "Koromaru", "Sans e e e e e"
+    };
+
+    for (const auto& clave : clavesComunes) {
+      std::string result;
+      for (int i = 0; i < cifrado.size(); i++) {
+        result += static_cast<unsigned char>(cifrado[i] ^ clave[i & clave.size()]);
+      }
+
+      if (isValidText(result)) {
+        cout << "============================\n";
+        cout << "Clave de diccionario: " << clave << "\n";
+        cout << "Texto posible: " << result << "\n";
+      }
+    }
+  }
+
 
 private:
 
--- SeguridadVJ/SeguridadVJ/src/main.cpp
+++ SeguridadVJ/SeguridadVJ/src/main.cpp
@@ -3,23 +3,36 @@
 
 int
 main() {
-  string texto;
-  int desplazo;
+  string mensaje = "Sans e e e e e";
+  string clave = "clave";
 
-  /*cout << "Escribe el mensaje a encriptar: " << endl;;
-  getline(cin, texto);
+  XOREncoder XORencoder;
 
-  cout << "Escribe el valor de desplazamiento: " << endl;;
-  cin >> desplazo;
+  cout << "Mensaje original: " << mensaje << endl << "\n";
 
-  CaesarEncryption cifrado;
-  string mensjaeCifrado = cifrado.EncryptionCaesar(texto, desplazo);
-  cout << "El mensaje cifrado es: " << mensjaeCifrado << endl;
+  cout << endl;
+  string cifrado = XORencoder.encode(mensaje, clave);
+  cout << "Mensaje cifrado: " << cifrado << endl;
+  cout << endl;
 
-  string descifrado = cifrado.DecodeCaesar(mensjaeCifrado, desplazo);
-  cout << "El mensaje descifrado es: " << descifrado << endl*/;
+  cout << "Texto cifrado (hex): ";
+  XORencoder.printHex(cifrado);
+  cout << endl;
+  cout << endl << "\n";
 
+  string descifrado = XORencoder.encode(cifrado, clave);
+  cout << "Mensaje descifrado: " << descifrado << std::endl;
 
+  vector<unsigned char> bytesCifrados(cifrado.begin(), cifrado.end());
+
+  cout << "\n--- Fuerza bruta (1 byte) con filtro ---\n";
+  XORencoder.bruteForce_1Byte(bytesCifrados);
+
+  cout << "\n--- Fuerza bruta (2 byte) con filtro ---\n";
+  XORencoder.bruteForce_2Byte(bytesCifrados);
+
+  cout << "\n--- Fuerza bruta (diccionario de claves) ---\n";
+  XORencoder.bruteForceByDictionary(bytesCifrados);
 
   return 0;
 }
