af56d453e0769fdf436efc391884f7762c7ef3bc
Luis R <78448904+Starmise@users.noreply.github.com>
2025-06-06 18:17:09 -0600

    DES Part 3
    Part 2 was included in the commit "DES Implemented"

--- SeguridadVJ/SeguridadVJ/src/main.cpp
+++ SeguridadVJ/SeguridadVJ/src/main.cpp
@@ -3,12 +3,37 @@
 
 int
 main() {
-  std::bitset<64> plaintext("0001001000110100010101100111100010011010101111001101111011110001");
+  // std::bitset<64> plaintext("0001001000110100010101100111100010011010101111001101111011110001");
   std::bitset<64> key("0001001100110100010101110111100110011011101111001101111111110001");
 
+  string phrase = "$good_faith$";
+
+  // Asegurar que la frase tenga longitus múltiplo de 8
+  while (phrase.size() % 8 != 0) {
+    phrase += '\0'; // Espacio vacio
+  }
+
+  vector<bitset<64>> cipherBlocks;
+  cout << "Texto origunal" << phrase << endl;
+  cout << "Cifrado en hexadecimal: " << endl;
+
+  // Cifrado DES
   DES des(key);
-  bitset<64> ciphertext = des.encode(plaintext);
-  cout << "Cifrado: " << ciphertext << endl;
+  for (size_t i = 0; i < phrase.size(); i += 8) {
+    string block = phrase.substr(i, 8);
+    auto blockBits = des.stringToBitset64(block);
+    auto encrypted = des.encode(blockBits);
+    cipherBlocks.push_back(encrypted);
+    cout << std::hex << std::uppercase << std::setw(16) << std::setfill('0') << encrypted.to_ullong() << " ";
+  }
+
+  // Descifrado DES
+  string output;
+  for (const auto& block : cipherBlocks) {
+    auto decrypted = des.decode(block);
+    output += des.bitset64ToString(decrypted);
+  }
+  cout << "\n""Texto descifrado: " << phrase << endl;
 
   return 0;
 }
