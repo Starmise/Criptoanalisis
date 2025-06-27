f4a146ded0cdcb1da732de5661e983cd10feabb1
Luis R <78448904+Starmise@users.noreply.github.com>
2025-06-27 08:52:01 -0600

    CryptoGenerator Part 2
    Implemented methods in CryptoGenerator for hex/base64 encoding, key, IV, and salt generation. Updated main.cpp to demonstrate usage of these new utilities, including printing random bytes, keys, IVs, and salts in various formats.

--- SeguridadVJ/SeguridadVJ/src/main.cpp
+++ SeguridadVJ/SeguridadVJ/src/main.cpp
@@ -8,7 +8,19 @@ main() {
 
   // 2) Genereate 16 generic random bytes
   auto randomBytes = cryptoGen.generateBytes(16);
-  //std::cout << "Random Bytes (hex): " <<< cryptoGen.toHex(randomBytes) << std::endl;
+  std::cout << "Random Bytes (hex): " << cryptoGen.toHex(randomBytes) << std::endl;
+
+  // 3) AES Key of 128 buts
+  auto key128 = cryptoGen.generateKey(128);
+  std::cout << "Key 128-bit (hex): "<< cryptoGen.toHex(key128) << std::endl;
+
+  // 4) IV of 128 bits (16 bytes)
+  auto iv = cryptoGen.generateIV(16);
+  std::cout << "IV 128-bit (hex): " << cryptoGen.toHex(iv) << "\n";
+
+  // 5) Salt of 16 bytes
+  auto salt = cryptoGen.generateSalt(8);
+  std::cout << "Salt (Base64): " << cryptoGen.toBase64(salt) << "\n";
 
   return 0;
 }
